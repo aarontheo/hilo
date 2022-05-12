a16b445835e3a8203beaad4aa812e9b8ad75f9e7
coolness10101 <aaron@missionarymall.org>
2022-05-12 11:08:13 -0600

    implemented Card class w/ Suit+Rank enums, faceUp bool, and constructors

--- Card.cs
+++ Card.cs
@@ -1,11 +1,32 @@
 using System;
 
 namespace hilo
-{ 
-    public class Card { 
-        public Card() 
-        { 
-            
-        } 
+{
+    public enum Suit
+    {
+        Spades, Clubs, Hearts, Diamonds
+    }
+    public enum Rank
+    {
+        Joker, Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King
+    }
+    public class Card
+    {
+        public Suit suit { get; set; }
+        public Rank rank { get; set; }
+        public Boolean faceUp { get; set; }
+        public Card(Rank rank, Suit suit = Suit.Spades,bool faceUp = false)
+        {
+            this.rank = rank;
+            this.suit = suit;
+            this.faceUp = faceUp;
+        }
+
+        public Card(int rank, Suit suit = Suit.Spades,bool faceUp = false)
+        {
+            this.rank = (Rank)rank;
+            this.suit = suit;
+            this.faceUp = faceUp;
+        }
     }
 }
