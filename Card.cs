47d5e934778e2343cae6de8fd700a473e6a23ec1
coolness10101 <aaron@missionarymall.org>
2022-05-14 18:56:55 -0600

    Basic gameplay loop

--- Card.cs
+++ Card.cs
@@ -12,21 +12,34 @@ namespace hilo
     }
     public class Card
     {
-        public Suit suit { get; set; }
-        public Rank rank { get; set; }
+        public Suit suit { get; }
+        public Rank rank { get; }
         public Boolean faceUp { get; set; }
-        public Card(Rank rank, Suit suit = Suit.Spades,bool faceUp = false)
+        public string name { get; }
+        public int num { get; }
+        public Card(Rank rank, Suit suit = Suit.Spades, bool faceUp = false)
         {
             this.rank = rank;
             this.suit = suit;
             this.faceUp = faceUp;
+            this.name = $"{this.rank} of {this.suit}";
+            this.num = (int)this.rank;
         }
 
-        public Card(int rank, Suit suit = Suit.Spades,bool faceUp = false)
+        public Card(int rank, Suit suit = Suit.Spades, bool faceUp = false)
         {
             this.rank = (Rank)rank;
             this.suit = suit;
             this.faceUp = faceUp;
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine(this.name);
+        }
+        public void PrintNum()
+        {
+            Console.WriteLine(this.num);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -19,10 +19,29 @@ namespace hilo
     {
         static void Main(string[] args)
         {
+            //goto this point to start a new game
+            newgame:
             int score = 300;
             Deck deck = new Deck();
+            Card currentCard;
+            //Deck discard = new Deck();
             deck.Populate();
             deck.Shuffle();
+            deck.PrintNums();
+            //Print an introduction for the player on the first run
+            currentCard = deck.draw();
+            Console.WriteLine(currentCard.name);
+            //goto this point to start a new round
+            newround:
+            for (int i = 0; i<13; i++)
+            {
+                //take the first card from the deck and draw it from the deck into currentCard
+                currentCard = deck.draw();
+                //display the current card
+                Console.WriteLine($"Current card: {currentCard.name}\nValue: {currentCard.num}");
+            }
+            //if the user has points left, prompt a new round
+            //else, declare their winnings and thank them for playing
         }
     }
 }
